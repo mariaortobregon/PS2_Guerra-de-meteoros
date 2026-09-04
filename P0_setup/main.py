6a31d9a4e69f4a8ffb5950b77c2be3bcc98a2539
pilar ortobre <pilitagames2012@gmail.com>
2026-09-04 12:31:33 -0600

    hoy

--- P0_setup/main.py
+++ P0_setup/main.py
@@ -8,8 +8,8 @@ import sys
 pygame.init()
 
 # TODO 1: Define el ancho y alto de la ventana (sugerencia: 800 x 400)
-ANCHO = None  # <-- reemplaza None por un número
-ALTO = None   # <-- reemplaza None por un número
+ANCHO = 250  # <-- reemplaza None por un número
+ALTO = 250   # <-- reemplaza None por un número
 
 pantalla = pygame.display.set_mode((ANCHO, ALTO))
 pygame.display.set_caption("Esquiva los Meteoritos")
@@ -18,11 +18,11 @@ reloj = pygame.time.Clock()
 
 # TODO 2: Define los cuadros por segundo (FPS) a los que correrá el juego.
 # Sugerencia: 60
-FPS = None
+FPS = 50
 
 # TODO 3: Define un color de fondo en formato RGB: una tupla de 3 números (0-255)
 # Ejemplo de morado oscuro: (30, 20, 50)
-COLOR_FONDO = None
+COLOR_FONDO = (60, 30, 90)
 
 corriendo = True
 while corriendo:
--- P1_movimiento/main.py
+++ P1_movimiento/main.py
@@ -13,14 +13,14 @@ pygame.display.set_caption("Esquiva los Meteoritos")
 reloj = pygame.time.Clock()
 FPS = 60
 
-COLOR_FONDO = (20, 20, 40)
-COLOR_JUGADOR = (0, 200, 255)
+COLOR_FONDO = (70, 40, 90)
+COLOR_JUGADOR = (0, 0, 255)
 
 # El jugador es un rectángulo que se mueve horizontalmente cerca del fondo
 JUGADOR_ANCHO, JUGADOR_ALTO = 50, 20
 jugador_x = ANCHO // 2 - JUGADOR_ANCHO // 2
 jugador_y = ALTO - 50
-VELOCIDAD_JUGADOR = 6  # píxeles que se mueve por cuadro
+VELOCIDAD_JUGADOR = 10  # píxeles que se mueve por cuadro
 
 corriendo = True
 while corriendo:
@@ -30,19 +30,24 @@ while corriendo:
 
     # TODO 1: Obtén el estado de todas las teclas presionadas.
     # Pista: pygame.key.get_pressed() devuelve una lista de booleanos.
-    teclas = None
+    teclas = pygame.key.get_pressed()
 
     # TODO 2: Si se presiona la flecha izquierda o "A", disminuye jugador_x
     # (recuerda que en pygame, x=0 es el borde izquierdo)
     # Pista: teclas[pygame.K_LEFT] o teclas[pygame.K_a]
-
+    if teclas[pygame.K_LEFT] or teclas[pygame.K_a]:
+        jugador_x -= VELOCIDAD_JUGADOR
     # TODO 3: Si se presiona la flecha derecha o "D", aumenta jugador_x
     # Pista: teclas[pygame.K_RIGHT] o teclas[pygame.K_d]
-
+    if teclas[pygame.K_RIGHT] or teclas[pygame.K_d]:
+            jugador_x += VELOCIDAD_JUGADOR
     # TODO 4: Evita que el jugador se salga de la pantalla por los lados.
     # Pista: usa dos condicionales que comparen jugador_x con 0
     # y con (ANCHO - JUGADOR_ANCHO)
-
+    if jugador_x < 0:
+         jugador_x = 800 - JUGADOR_ANCHO
+    if jugador_x > 800 - JUGADOR_ANCHO:
+         jugador_x = JUGADOR_ANCHO
     pantalla.fill(COLOR_FONDO)
     pygame.draw.rect(
         pantalla, COLOR_JUGADOR, (jugador_x, jugador_y, JUGADOR_ANCHO, JUGADOR_ALTO)
